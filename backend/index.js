4ba221e4bf2df8c9f4f11d6d6c3e68ca21e1f528
randolf <136251983+RangePackaging@users.noreply.github.com>
2024-10-10 19:17:19 -0700

    Finished with creating and getting  Product Route

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,10 @@ app.use(cors({
 // all routs
 
 const authRoutes = require('./src/users/user.route');
+const productRoutes = require('./src/products/products.route');
+
 app.use('/api/auth', authRoutes)
+app.use('/api/products', productRoutes)
 
 
 
---
+++ backend/src/products/products.route.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const Products = require('./products.model');
+const Reviews = require('../reviews/reviews.model');
+const router = express.Router();
+
+// post a product
+router.post('/create-product', async (req, res) => {
+    try {
+        const newProduct = new Products({
+            ...req.body
+        });
+
+        const savedProduct = await newProduct.save();
+
+        //Calculate reviews
+        const reviews = await Reviews.find({productId: savedProduct._id});
+        if(reviews.length > 0) {
+            const totalRating = reviews.reduce((acc, review) => acc + review.rating, 0);
+            const averageRating = totalRating / reviews.length;
+            savedProduct.rating = averageRating;
+            await savedProduct.save();
+        }
+        res.status(201).send(savedProduct);
+    } catch (error) {
+        console.error('Error creating new product', error);
+        res.status(500).send({message: 'Failed to create new product'})
+
+    }
+});
+
+
+// get all products
+router.get('/', async (req, res) => {
+    try {
+        const {category, color, minPrice, maxPrice, page=1, limit=10} = req.query;
+
+        let filter = {};
+        if(category && category !== 'all') {
+            filter.category = category
+        }
+
+        if(color && color !== 'all') {
+            filter.color = color
+        }
+
+        if(minPrice && maxPrice !== 'all') {
+            const min = parseFloat(minPrice)
+            const max = parseFloat(maxPrice)
+            if (!isNaN(min) && !isNaN(max)) {
+                filter.price = {$gte: min, $lte: max};
+            }
+        }
+        const skip = (parseInt(page) - 1) * parseInt(limit);
+        const totalProducts = await Products.countDocuments(filter);
+        const totalPages = Math.ceil(totalProducts / parseInt(limit));
+        const products = await Products.find(filter).skip().limit(parseInt(limit)).populate('author', 'email').sort({createdAt: -1});
+        
+        res.status(200).send({products, totalPages, totalProducts})
+
+
+    } catch (error) {
+        console.error('Error Fetching products', error);
+        res.status(500).send({message: 'Failed to create new product'})
+    }
+})
+
+module.exports = router
